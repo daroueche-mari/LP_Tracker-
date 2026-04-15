22b98a0051c8c1dbd824acc31abf9f3735acc4e8
Daroueche MARI <daroueche.mari@laplateforme.io>
2026-04-15 10:55:21 +0200

    Final Checking LP Tracker Student Management

--- database.sql
+++ database.sql
@@ -6,3 +6,10 @@ CREATE TABLE student (
     age INT,
     grade DECIMAL(4,2) -- Permet de stocker des notes comme 15.50
 );
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY, -- SERIAL correspond au int4 auto-incrémenté
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    salt VARCHAR(255),
+    avatar_url VARCHAR(255)
+);
